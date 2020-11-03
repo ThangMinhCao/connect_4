c203aa0accde25e04fa5960633230b9ee0f332a2
ThangMinhCao <thangminhcao@gmail.com>
2020-11-03 17:18:41 -0500

    Back to good version

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo systemctl enable mongod
-sudo systemctl start mongod 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-npm install
-npm audit fix 
